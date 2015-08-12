69da3c92bb81db13cdf1f5ec6980bb063ac28b51
Afonasev Eugene <ea.afonasev@gmail.com>
2015-08-12 20:01:29 +0500

    add example into events.__init__.py

--- events/__init__.py
+++ events/__init__.py
@@ -1,3 +1,26 @@
+"""Send/subscribe events library
+
+    import events
+
+    @events.subscribe('foo')
+    def foo_handler(count):
+        print(count)
+
+    def foo_handler_2(count):
+        print(count)
+
+    events.subscribe('foo', foo_handler_2)
+    events.send('foo', 3)
+
+
+    events.set_default_registry(events.EventRegistry(
+        auto_register=False,
+    ))
+
+    events.register('foo')
+    events.send('foo', 3)
+"""
+
 from .events import register, send, subscribe, set_default_registry
 from .events import IExecutor, SyncExecutor, AsyncExecutor
 from .events import IEventRegistry, EventRegistry
